ef8c490e63b4a1e6df814a7addba2865a20fc769
fmendez <45771236+fmendez884@users.noreply.github.com>
2023-10-03 21:09:42 -0400

    remove mobile preprosser directives

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -147,7 +147,7 @@ namespace StarterAssetsTPController
 // #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
             _playerInput = GetComponent<PlayerInput>();
 // #else
-			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
+			// Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 // #endif
 
             AssignAnimationIDs();
